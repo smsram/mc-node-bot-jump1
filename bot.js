fd4688330414f01d89762c456beb5076651967d0
S Meher Siva Ram <smsram001@gmail.com>
2025-01-27 19:45:11 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+// Remove all listeners for warning events to suppress deprecation warnings
+process.removeAllListeners('warning'); 
+
 const mineflayer = require('mineflayer');
 
 let bot; // Global bot instance
@@ -7,10 +10,10 @@ let reconnecting = false; // To prevent multiple reconnect attempts
 function createBot() {
   bot = mineflayer.createBot({
     host: 'smsram.aternos.me', // Replace with your Aternos server IP
-    port: 48121,                      // Use your specific port
-    username: `Bot_Gadu`, // Random username
-    version: '1.21.4',                // Specify the exact version of the server
-    auth: 'offline',                  // Use offline auth for cracked servers
+    port: 48121,               // Use your specific port
+    username: `Bot_Gadu`,      // Random username
+    version: '1.21.4',         // Specify the exact version of the server
+    auth: 'offline',           // Use offline auth for cracked servers
   });
 
   bot.on('spawn', () => {
